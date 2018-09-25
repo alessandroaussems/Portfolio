6a69f77be0cda0bc0f853e8dd1d4db07284dbb46
alessandroaussems <alessandrossms@gmail.com>
2018-09-25 21:05:14 +0200

    ChangedDeploy=>SwitchedFromHost

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 #Zipping files in /htdocs to release.zip
 cd htdocs && zip -r ../release.zip . && cd ..
 #Copy release.zip to live server
-scp release.zip alessandroaussemsbe@ssh.alessandroaussems.be:www/release.zip
+scp -P 7685 release.zip u38481p33797@web0092.zxcs.be:domains/alessandroaussems.be/public_html/release.zip
 #SSH to live server browse to directory, unzip release in the current directory and remove release.zip
-ssh alessandroaussemsbe@ssh.alessandroaussems.be "cd www && unzip -o release.zip && rm -rf release.zip"
+ssh u38481p33797@web0092.zxcs.be -p 7685 "cd domains/alessandroaussems.be/public_html/ && unzip -o release.zip && rm -rf release.zip"
 #Clean-up remove release.zip from local system
 rm -rf release.zip
